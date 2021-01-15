ffbcac4aac66df9b12092bd7468b3eddae71a770
kazi-rakib-uddin <kazisane@gmail.com>
2021-01-15 21:01:12 +0530

    hello world

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,27 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-
-=======
-
-public class MainActivity extends AppCompatActivity {
-
->>>>>>> 9fd6b42 (hello world)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 9fd6b42 (hello world)
     }
 }
